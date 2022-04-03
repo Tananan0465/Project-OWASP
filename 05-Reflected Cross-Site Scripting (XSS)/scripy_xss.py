afbb8ab8dc6ae583d04e250fc8076c88a0d87077
Tananan0465 <61010465@kmitl.ac.th>
2022-04-04 00:31:48 +0700

    add script_xss

---
+++ 05-Reflected Cross-Site Scripting (XSS)/scripy_xss.py
@@ -0,0 +1,17 @@
+from subprocess import PIPE, run
+import os
+import time
+import subprocess
+
+def XSS():
+    XSSResult = open("XSSResult.txt", "w+")
+    for line in open("dnsNoDup.txt", "r"):
+        subprocess1 = subprocess.Popen("python xsssniper.py -u '"+line+"' --crawl --forms", shell=True, stdout=subprocess.PIPE)
+        subprocess_return = subprocess1.stdout.read()
+        #print(subprocess_return)
+        XSSResult.write(subprocess_return.decode("utf-8"))
+    XSSResult.close()
+
+
+
+XSS()
